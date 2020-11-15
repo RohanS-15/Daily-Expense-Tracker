5a56bf238524045631889ef5607f96759d9c6861
Rohan <rohan15sylvain@gmail.com>
2020-11-15 13:30:48 -0700

    made the raw welcome page and tweaked a couple other things

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # DailyExpenseTracker
 
-Daily Expense Tracker app. In active development
+Daily Expense Tracker app. In active development. Main Code is in lib directory.
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(App());
-
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
+void main(){
+  runApp(
+    MaterialApp(
       title: "Daily Expense Tracker",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Daily Expense Tracker"),
-        ),
-        body: Center(
-          child: Text("Daily Expense Tracker"),
-        ),
-      ),
+      home: WelcomePage(),
+    )
+  );
+}
+
+class WelcomePage extends StatelessWidget{
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.purple[300],
+      body: Center(
+        child: Text("Welcome the your expense tracker."),
+
+      )
     );
   }
 }
+
