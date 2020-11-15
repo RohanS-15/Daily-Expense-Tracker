8e74fdb5cfedecc9a89e336ad9a7e0851449c446
Rohan <rohan15sylvain@gmail.com>
2020-11-14 18:33:40 -0700

    modified readme

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Daily Expense Tracker",
       home: Scaffold(
         appBar: AppBar(
