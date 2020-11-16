089c46a128b472b147c2440a9e91861a843894b1
Rohan <rohan15sylvain@gmail.com>
2020-11-15 19:47:14 -0700

    made the navigation look better

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MainPage extends StatelessWidget{
   @override 
   Widget build(BuildContext context){
-    return Scaffold( 
-      body: Center(child: 
-        ElevatedButton( 
-          child: Text("Go back"),
-          onPressed: (){
-            Navigator.pop(context);
-          },
-        )
+    return Scaffold(
+      appBar: AppBar( 
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
+        backgroundColor: Colors.white60,
+        title: Text(
+          "Daily Expense Tracker",
+          style: GoogleFonts.raleway(
+            textStyle: TextStyle(fontSize: 25, color: Colors.black)
+          ),
+        ),
+      ), 
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => print("Hello world"),
+        backgroundColor: Colors.greenAccent[400],
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      body: Column(children: [
+        Text("Crap")
+      ],)
     );
   }
 }
--- lib/welcomePage.dart
+++ lib/welcomePage.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:gradient_widgets/gradient_widgets.dart';
 import 'mainPage.dart' as mainPage;
 
 class WelcomePage extends StatelessWidget{
   @override 
   Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: Colors.teal[400],
+      backgroundColor: Colors.indigoAccent[100],
       body: Center(child: 
         Column(children: [
           Text(
-            "Own your money.",
+            "Welcome to the daily expense tracker\n",
             style: GoogleFonts.raleway(
-              textStyle: TextStyle(fontSize: 30)
-            )
-          ),
-          ElevatedButton( 
-            child: Text("Get Started"),
+              textStyle: TextStyle(fontSize: 34)
+            ),
+            textAlign: TextAlign.center,
             
-            onPressed:(){
+          ),
+          GradientButton( 
+            increaseHeightBy: 10,
+            increaseWidthBy: 30,
+            gradient: Gradients.coldLinear,
+            child: Text(
+              "Continue",
+              style: TextStyle(
+                color: Colors.black, 
+                fontSize: 20, 
+                fontWeight: FontWeight.w400),
+            ),
+            callback:(){
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => mainPage.MainPage())
